87661190a37d695e99a8acbd6e4e24bcd065930e
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-05-22 17:14:19 +0200

    re-import cards correctly after move

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import Cards from "../../components/cards/card";
-import { CardsWrap } from "../../components/cards/cardwrapper";
 import { PageWrap } from "../../components/pagewrapp/style";
+import Cards from "../../components/cards/card";
 
 export default function Home() {
     return(
         <PageWrap>
           <h1>HOME</h1>
-          <CardsWrap>
-            <h2><i className="fa-solid fa-fire"></i> items:</h2>
-            <Cards/>
-          </CardsWrap>
+          <h2><i className="fa-solid fa-fire"></i> items:</h2>
+          <Cards/>
         </PageWrap>
     )
 }
