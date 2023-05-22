fba9fe9ddeafc908103701bb076bcda36429f7d9
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-05-22 20:13:58 +0200

    adds try&catch blocks to Cards()

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -5,29 +5,31 @@ import { Image, CardsWrapp, CardWrap } from "./style";
 
 export default function Cards() {
 
-  const [data, setData] = useState([]);
-  const endpoint = "https://api.noroff.dev/api/v1/online-shop";
-
-  async function getData() {
-    const response = await fetch(endpoint);
-    setData(await response.json());
-  }
-  useEffect(() => {
-    getData();
-  }, []);
-
-return (
-<CardsWrapp>
-  {data.map((data) => {
-    return(
-    <CardWrap key={data.id}>
-        <Link to={`product/${data.id}`}>
-            <h2>{data.title}</h2>
-            <Image src={data.imageUrl}></Image>
-        </Link>
-    </CardWrap>
-    )
-  })}
-</CardsWrapp>
-)
+  try {
+    const [data, setData] = useState([]);
+    const endpoint = "https://api.noroff.dev/api/v1/online-shop";
+  
+    async function getData() {
+      const response = await fetch(endpoint);
+      setData(await response.json());
+    }
+    useEffect(() => {
+      getData();
+    }, []);
+  
+  return (
+  <CardsWrapp>
+    {data.map((data) => {
+      return(
+      <CardWrap key={data.id}>
+          <Link to={`product/${data.id}`}>
+              <h2>{data.title}</h2>
+              <Image src={data.imageUrl}></Image>
+          </Link>
+      </CardWrap>
+      )
+    })}
+  </CardsWrapp>
+  )
+  } catch(error) {console.log(error)}
 }
