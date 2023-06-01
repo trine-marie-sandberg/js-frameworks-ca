4dcc4c47ffe0d72ed7c7ee1b26ae1708ba0ddf3e
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-06-01 19:57:03 +0200

    adds cartfunctions to btns and updates styling

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { PageWrap } from "../../components/pagewrapp/style";
 import * as storage from "../../hooks/storage";
 import { Link } from "react-router-dom";
-import { CartWrap, Image } from "./style";
+import { CartWrap, CartContainer, Image, SmallText } from "./style";
 import { useNavigate } from "react-router-dom";
+import { cartBtns } from "../../hooks/cartfunctions";
 
 export default function Cart() {
     const navigate = useNavigate();
@@ -15,23 +16,32 @@ export default function Cart() {
     for(let i = 0; i < objectKeys.length; i++) {
         cartArray.push(storage.load(objectKeys[i]));
     }
+    
     console.log(cartArray)
     //console.log(cartItems)
     return(
         <PageWrap>
-            <h1>CART</h1>
             <div>
-                {cartArray.map((data) => {
-                    return(
-                        <CartWrap key={data.id}>
-                            <div  onClick={() => navigate(`/product/${data.id}`)} aria-label="Back button">
-                              <h2>{data.title}</h2>
-                              <p>{data.price}</p>
-                              <Image src={data.imgUrl} alt={data.title}></Image>
-                            </div>
-                        </CartWrap>
-                    )
-                })}
+                <h1>My cart</h1>
+                <CartContainer>
+                    {cartArray.map((data) => {
+                        const cartBtn = cartBtns(data.id, data.title, data.price, data.imgUrl);
+                        return(
+                            <CartWrap key={data.id}>
+                                <button onClick={cartBtn.add}>+</button>
+                                <button onClick={cartBtn.remove}>-</button>
+                                <button onClick={cartBtn.clear}>Remove</button> 
+                                {cartBtn.displayCount}
+                                <div  onClick={() => navigate(`/product/${data.id}`)} aria-label="Back button">
+                                <SmallText>{data.title}</SmallText>
+                                <p>${data.price}</p>
+                                <Image src={data.imgUrl} alt={data.title}></Image>
+                                </div>
+                            </CartWrap>
+                        )
+                    })}
+                </CartContainer>
+                <h2>Checkout</h2>
             </div>
         </PageWrap>
     )
--- src/pages/cart/style.jsx
+++ src/pages/cart/style.jsx
@@ -2,17 +2,37 @@ import { styled } from "styled-components";
 
 export const CartWrap = styled.div`
   padding:  ${(props) => props.theme.sizes.sm};
-  background-color: ${(props) => props.theme.color.primary};
+  margin: 5px;
+  max-height: 250px;
+  cursor: pointer;
   color: ${(props) => props.theme.color.secondary};
   border-radius: ${(props) => props.theme.sizes.borderRadius};
-  border: 1px solid ${(props) => props.theme.color.secondary};
-  width: 10vw;
+  background-color: ${(props) => props.theme.color.primary};
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.562) 0px 5px 15px;
+  }
+`;
+export const CartContainer = styled.div`
+  max-height: 250px;
+  overflow-y: scroll;
+  display: grid;
+  grid-template-columns:  repeat( auto-fill, minmax(190px, 1fr) );
+  grid-column-gap: 6px;
+  grid-row-gap: 6px;
+  padding:  ${(props) => props.theme.sizes.sm};
+  border-radius: ${(props) => props.theme.sizes.borderRadius};
+  background-color: ${(props) => props.theme.color.darker};
+  text-decoration: none;
 `;
 export const Icon = styled.i`
   font-size: ${(props) => props.theme.sizes.sm};
 `;
 export const Image = styled.img`
-  height: auto;
-  width: 100%;
+  height: 100%;
+  max-height: 100px;
+  width: auto;
   border-radius: ${(props) => props.theme.sizes.borderRadius};
+`;
+export const SmallText = styled.h2`
+  font-size: ${(props) => props.theme.sizes.sm};
 `;
