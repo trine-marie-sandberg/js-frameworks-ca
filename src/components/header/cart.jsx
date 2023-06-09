670abb0cea590f2d15389998aebfbcc00231e6b1
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-06-09 12:34:18 +0200

    formatting

--- src/components/header/cart.jsx
+++ src/components/header/cart.jsx
@@ -1,36 +1,30 @@
 import React, {useState, useEffect} from "react";
 import { NavLink } from "react-router-dom";
 import { I, CartWrapper, CartItemsIndikator } from "./style";
-import * as storage from "../../hooks/storage";
 
 export function CartIcon() {
-    //FORTSETT FRA I GÅR:
-    //https://www.youtube.com/watch?v=IK6KXIkJZqU 
-    //https://www.youtube.com/watch?v=jWWW9Wyl0mY
-    //https://react.dev/reference/react/Component#catching-rendering-errors-with-an-error-boundary
 
     const [storageState, setStorageState] = useState(0);
-    const [itemsCount, setItemsCount] = useState(0);
+    const [itemsCount, setItemsCount] = useState([]);
 
     const cartItems = {...localStorage};
     let objectKeys = Object.keys(cartItems);
     let itemsInCart = objectKeys.length;
 
-    window.addEventListener("storage", (event) => {
+    window.addEventListener("storage", () => {
         //Re-render when changes to localstorage is made
         let newStorageState = storageState + 1;
         setStorageState(newStorageState);
 
         itemsInCart = objectKeys.length;
-        console.log(itemsInCart)
-        setItemsCount(itemsInCart)
+        //console.log(`Count inside eventlistener: ${itemsInCart}`)
     })
 
     useEffect(() => {
-        setItemsCount(itemsInCart)
-        console.log("dependency array working!")
+        setItemsCount(itemsInCart);
     }, [storageState])
-    console.log(itemsInCart)
+
+    //console.log(`final count: ${itemsInCart}`)
     return(
         <NavLink to="cart" aria-label="Cart">
             <CartWrapper>
