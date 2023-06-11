a6e482990c7f386a1c10d14eb5326c40ec2ce837
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-06-11 18:04:13 +0200

    updates footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,6 +3,6 @@ import { Foot } from "./style";
 
 export default function Footer() {
     return(
-        <Foot>© 2023, rights reserved</Foot>
+        <Foot>© 2023, rights reserved. Created with Vite + React</Foot>
     )
 }
